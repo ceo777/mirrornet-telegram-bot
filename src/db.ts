7188b378ff3af5f46b7882780c1aa1232fffc490
Oleg Dubnov <61102565+ceo777@users.noreply.github.com>
2024-01-07 06:13:49 +0300

    MongoDB connection module was added

---
+++ src/db.ts
@@ -0,0 +1,42 @@
+/** Mongo Database connection */
+
+import 'dotenv/config';
+import { MongoClient } from 'mongodb';
+
+/** */
+export default class MongoDB {
+    /**
+     * Connection URI. See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    private readonly DB_URI: string;
+
+    /**
+     * The Mongo Client you will use to interact with your database.
+     * See https://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html for more details.
+     * In case: '[MONGODB DRIVER] Warning: Current Server Discovery and Monitoring engine is deprecated...'
+     * pass option { useUnifiedTopology: true } to the MongoClient constructor.
+     * const client =  new MongoClient(uri, {useUnifiedTopology: true})
+     */
+    public readonly client: MongoClient;
+
+    constructor() {
+        if (process.env.DB_USERNAME && process.env.DB_PASSWORD) {
+            this.DB_URI = 'mongodb://' +
+                process.env.DB_USERNAME + ':' +
+                process.env.DB_PASSWORD + '@' +
+                process.env.DB_HOST + ':' +
+                process.env.DB_PORT + '/' +
+                process.env.DB_NAME + '?' +
+                process.env.DB_ARGS;
+        } else {
+            this.DB_URI = 'mongodb://' +
+                process.env.DB_HOST + ':' +
+                process.env.DB_PORT + '/' +
+                process.env.DB_NAME + '?' +
+                process.env.DB_ARGS;
+        }
+        this.client = new MongoClient(this.DB_URI, { monitorCommands: true });
+        console.log('MongoDB connection has been established.');
+        // this.client.on('commandStarted', started => console.log(started));
+    }
+}
